8f14bad898db3e75df008557f00dda3959a1658a
Perapats <perapat.pcrt@gmail.com>
2025-04-25 03:21:51 +0700

    Portfolio Files

---
+++ Website/index_files/pages/js/blogpost_en.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    document.body.classList.add('fade-in');
+
+    const themeToggle = document.getElementById("theme-toggle");
+    const themeIcon = document.getElementById("theme-icon");
+
+    themeToggle.addEventListener("click", () => {
+        document.body.classList.toggle("dark-mode");
+        themeIcon.src = document.body.classList.contains("dark-mode")
+            ? "pics/light.png"
+            : "pics/dark.png";
+
+        if (document.body.classList.contains("dark-mode")) {
+            localStorage.setItem("darkMode", "enabled");
+        } else {
+            localStorage.setItem("darkMode", "disabled");
+        }
+    });
+
+    
+    function loadDarkMode() {
+        const darkMode = localStorage.getItem("darkMode");
+        if (darkMode === "enabled") {
+            document.body.classList.add("dark-mode");
+        }
+    }
+    loadDarkMode(); 
+
+    const languageBtn = document.querySelector(".language-btn");
+    languageBtn.addEventListener("click", (event) => {
+        event.preventDefault();
+        document.body.classList.add("fade-out");
+        setTimeout(() => {
+            window.location.href = languageBtn.href;
+        }, 500);
+    });
+
+    let lastScrollTop = 0;
+    const header = document.querySelector("header");
+    let isScrolling;
+
+    window.addEventListener("scroll", () => {
+        const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+        header.style.top = scrollTop > lastScrollTop ? "-100px" : "0";
+        lastScrollTop = scrollTop;
+
+        clearTimeout(isScrolling);
+        isScrolling = setTimeout(() => {}, 150);
+    });
+});
